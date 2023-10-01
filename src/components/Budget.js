c01792f32f31edcaa89cfa82716117beb7500e06
Rustam-Asdanov <asdanovrustam@gmail.com>
2023-10-01 07:39:37 +0200

    some updates

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,25 +1,26 @@
-
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
-    
-    return (
-            <div className='alert alert-secondary'>
-                <span>Budget: £{budget}</span>
-                <input 
-                    type="number" 
-                    step="10" 
-                    value={newBudget} 
-                    onChange={handleBudgetChange}>
-                </input>
-            </div>
-        );
-    };
+  const { budget } = useContext(AppContext);
+  const [newBudget, setNewBudget] = useState(budget);
+
+  const handleBudgetChange = (event) => {
+    setNewBudget(event.target.value);
+  };
+
+  return (
+    <div className="alert alert-secondary">
+      <span>Budget: £{budget}</span>
+      <input
+        type="number"
+        step="10"
+        max="20000"
+        value={newBudget}
+        onChange={handleBudgetChange}
+      ></input>
+    </div>
+  );
+};
 
 export default Budget;
